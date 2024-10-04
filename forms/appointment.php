5ec42503ea55134de79587eca24f182045a6169a
Harlem-Bryan <bryan.cardel@maverickvirtual.com>
2024-10-05 05:14:47 +0800

    Changes on Appointment Section

--- forms/appointment.php
+++ forms/appointment.php
@@ -37,7 +37,7 @@
   $contact->add_message( $_POST['email'], 'Email');
   $contact->add_message( $_POST['phone'], 'Phone');
   $contact->add_message( $_POST['date'], 'Appointment Date');
-  $contact->add_message( $_POST['department'], 'Department');
+  $contact->add_message( $_POST['program'], 'Program');
   $contact->add_message( $_POST['doctor'], 'Doctor');
   $contact->add_message( $_POST['message'], 'Message');
 
